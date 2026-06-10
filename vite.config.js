3f620e95564bab5e50b1d3d8e5b5d1fb7dcb61cf
Himanshurchaudhary <rchaudhary9771@gmail.com>
2026-06-10 13:09:29 +0530

    fix: add _redirects for React Router on Hostinger

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // base: '/graminkart/',
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    }
+  }
 })
